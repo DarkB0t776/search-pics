3a102c52a7b6e14a2415271e94dfa0e84cdd8d45
Dima <dima23790@gmail.com>
2020-04-09 21:33:15 +0300

    Updates

---
+++ precache-manifest.59e758fde0e3c335a6e46948bf4f70bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ac8fb9d37b9b64e571df80a756d6e2",
+    "url": "/search-pics/index.html"
+  },
+  {
+    "revision": "5323167637d990e35bba",
+    "url": "/search-pics/static/css/main.41d913fe.chunk.css"
+  },
+  {
+    "revision": "f2cdb5e906ffa92bf002",
+    "url": "/search-pics/static/js/2.18340ca6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/search-pics/static/js/2.18340ca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5323167637d990e35bba",
+    "url": "/search-pics/static/js/main.c3967e09.chunk.js"
+  },
+  {
+    "revision": "4c2d7627ef29f07d69a6",
+    "url": "/search-pics/static/js/runtime-main.30083b4f.js"
+  }
+]);
